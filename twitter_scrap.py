15488923a48523ed5ff822873912bfba6a7aa30b
MarouBen <maroumarocom@gmail.com>
2022-11-30 16:53:51 +0100

    Update twitter_scrap.py

--- twitter_scrap.py
+++ twitter_scrap.py
@@ -2,6 +2,7 @@ import tweepy as twe
 import pandas as pan
 import numpy as np
 from matplotlib import pyplot as ppt
+from project import Get_coin
 
 # Here are the four needed variables given by tweeter developer you can get yours if those dosent works
 Key = ""
@@ -14,3 +15,7 @@ authen = twe.OAuthHandler(Key,Secret)
 authen.set_access_token(Token,TokenSecret)
 api = twe.API(authen,wait_on_rate_limit = True)
 
+# Get tweets
+def Get_tweets(name):
+    key_word = f"#{name}"
+    
