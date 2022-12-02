0f4f1366791fff64e316518b95598ebea76db4b6
MarouBen <maroumarocom@gmail.com>
2022-12-02 20:26:40 +0100

    added a function that clean tweets

--- twitter_scrap.py
+++ twitter_scrap.py
@@ -1,10 +1,15 @@
 import tweepy as twe
 import pandas as pan
 import numpy as np
+import re
+import nltk
 from textblob import TextBlob
 import matplotlib.pyplot as ppt
 from project import Get_coin
-ppt.style.use("fivethirtyeight")
+
+nltk.download('stopwords')
+stopwords = nltk.corpus.stopwords.words('english')
+
 
 # Here are the four needed variables given by tweeter developer you can get yours if those dosent works
 Key = "bpWyda2RuIryjyHGLUOGM6QFK"
@@ -13,20 +18,21 @@ Token = "1597963175479201792-9GhrfziYt6Bsmm58BwYmGDelOumiLk"
 TokenSecret = "lqZOQwZvvyjkRPxW0EViQaaQrmoJGVe86NZtNXGGMy2M7"
 
 # Authentification 
-authen = twe.OAuth2AppHandler(Key,Secret)
+authen = twe.OAuthHandler(Key,Secret)
 authen.set_access_token(Token,TokenSecret)
 api = twe.API(authen,wait_on_rate_limit = True)
 
 def main():
     Tweets = Get_tweets("Bitcoin")
     df = Build_df(Tweets)
+    Build_plot(df,"Bitcoin")
 
 
 
 # Get tweets
 def Get_tweets(name):
-    keyword = f"#{name}"
-    Full_tweets = twe.Cursor(api.search_30_day, q=keyword)
+    keyword = f"#{name} -filter:retweets"
+    Full_tweets = twe.Cursor(api.search_tweets, q=keyword).items()
     # Filter just the tweets text
     Tweets = [{"Tweets":tweet.text} for tweet in Full_tweets]
     return Tweets
@@ -39,16 +45,15 @@ def Get_polarity(twt):
 # Function that gets the sentiment of the tweet
 def Get_sentiment(n):
     if n < 0:
-        return "negative"
+        return "NEG"
     elif n > 0 :
-        return "positive"
-    else:
-        return "neutral"
+        return "POS"
     
 # Functin that build the dataframe with the polarity and sentiment
 def Build_df(Tweets):
     # Creating panda dataframe
-    df = pan.DataFrame(Tweets)
+    df = pan.DataFrame.from_dict(Tweets)
+    df["CleanTweets"] = df["Tweets"].apply(Clean_tweets)
     df["Polarity"] = df["Tweets"].apply(Get_polarity)
     df["Sentiment"] = df["Polarity"].apply(Get_sentiment)
     return df
@@ -57,10 +62,25 @@ def Build_df(Tweets):
 def Build_plot(df,name):
     plot = df["Sentiment"].value_counts().plot(kind = "bar")
     ppt.title(f"Sentiment Analysis of {name}")
-    ppt.xlabel("Sentiment")
+    ppt.xlabel("Sentiment",rotation='horizontal')
     ppt.ylabel("Num of Tweets")
     ppt.savefig(f"Plots/Sentiment of {name}.pdf")
     return None
+
+
+def Clean_tweets(tweet):
+    tweet = tweet.lower()
+    tweet = re.sub("'", "", tweet) # to avoid removing contractions in english
+    tweet = re.sub("@[A-Za-z0-9_]+","", tweet) # Remove mentions
+    tweet= re.sub("#[A-Za-z0-9_]+","", tweet) # Remove any word that start with #
+    tweet = re.sub(r'https?\S+', '', tweet) # Remove links
+    tweet = re.sub('[()!?]', ' ', tweet) # Removing punctuations
+    tweet = re.sub('\[.*?\]',' ', tweet)
+    tweet = re.sub("[^a-z0-9]"," ", tweet) # Filtering non-alphanumeric characters
+    tweet = tweet.split() # Tokenization
+    tweet = [w for w in tweet if not w in stopwords] # remove stopwords
+    tweet = " ".join(word for word in tweet)
+    return tweet
 
 if __name__ == "__main__":
     main()
